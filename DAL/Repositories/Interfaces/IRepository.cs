bb53e668e523484d32120b01aef1398c4badc229
xairaven <alexkovalevkpi@gmail.com>
2023-12-06 02:34:04 +0200

    (DAL) Fix: Find method signature

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -4,7 +4,7 @@ public interface IRepository<T> where T : class
 {
     IEnumerable<T> GetAll();
     T Get(int id);
-    IEnumerable<T> Find(Func<T, bool> predicate);
+    IEnumerable<T> Find(Func<T, bool> predicate, int pageNumber = 0, int pageSize = 10);
     void Create(T item);
     void Update(T item);
     void Delete(int id);
