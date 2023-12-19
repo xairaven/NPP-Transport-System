4ae1401374c5a905b832f80b5a01b62f44cb55d4
xairaven <alexkovalevkpi@gmail.com>
2023-12-20 01:57:20 +0200

    (CCL) Feat: SecurityContext

---
+++ CCL/SecurityContext.cs
@@ -0,0 +1,18 @@
+﻿using CCL.Identity;
+
+namespace CCL;
+
+public static class SecurityContext
+{
+    private static User? _user;
+    
+    public static User? GetUser()
+    {
+        return _user;
+    }
+    
+    public static void SetUser(User user)
+    {
+        _user = user;
+    }
+}
